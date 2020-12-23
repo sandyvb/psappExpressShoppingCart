62852bc169fdbfc71a9548c6e9badb914f5d662d
Sandy <sandra@revoltwind.com>
2020-12-23 05:48:24 -0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import fetch from 'node-fetch'
+import axios from 'axios'
 
 function App() {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    sendProductData()
+    callBlockonomics()
+      .then((res) => setData(res.data))
+      .catch((err) => console.log('callBlockonomics error: ', err))
+  })
+
+  const sendProductData = async () => {
+    await axios
+      .post('/productData', {
+        title: 'the title',
+        productDescription: 'the description',
+        price: 2.99,
+        downloadLink: 'the download link',
+      })
+      .then((res) => console.log('data sent: ', res))
+      .catch((e) => console.log(e))
+  }
+
+  const callBlockonomics = async () => {
+    const response = await fetch('/data', { todo: 'buy milk' })
+    const body = await response.json()
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="App-intro">{data}</p>
+        <button className="blockoPayBtn" data-toggle="modal" data-uid={data}>
+          download
+        </button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// to run: from root/client: npm start
