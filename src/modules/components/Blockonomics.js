253a42072ab4dcde5c65f8011047fd872ac46501
Sandy <sandra@revoltwind.com>
2021-01-14 10:05:55 -0600

    about to implement shopping cart

--- src/modules/components/Blockonomics.js
+++ src/modules/components/Blockonomics.js
@@ -2,38 +2,13 @@ import React, { useState, useEffect } from 'react'
 import fetch from 'node-fetch'
 
 const Blockonomics = (props) => {
-  const [data, setData] = useState('')
+  const [data, setData] = useState('0')
+
   const title = props.title
   const productDescription = props.productDescription
   const price = parseFloat(props.price)
   const downloadLink = props.downloadLink || '0'
   const parentUid = 'c410a2f48e2311ea'
-
-  useEffect(() => {
-    callBlockonomics()
-      .then((res) => setData(res.data))
-      .catch((err) => console.log('callBlockonomics error: ', err))
-  })
-
-  const callBlockonomics = async () => {
-    const response = await fetch('https://app.powershotz.com/product', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        parent_uid: parentUid,
-        product_name: title,
-        product_description: productDescription,
-        value: price,
-        extra_data: downloadLink,
-      }),
-    })
-    const body = await response.json()
-
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
-    return body
-  }
 
   const styles = {
     button: {
@@ -42,6 +17,36 @@ const Blockonomics = (props) => {
       paddingLeft: '20px',
       paddingRight: '20px',
     },
+  }
+
+  useEffect(() => {
+    fetchData()
+    console.log(data)
+  })
+
+  async function fetchData() {
+    // await fetch('https://revoltwind.com/product', {
+    await fetch('/product', {
+      // fetch('/product', {
+      method: 'POST',
+      // mode: 'cors',
+      headers: {
+        // 'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        parent_uid: parentUid,
+        product_name: title,
+        product_description: productDescription,
+        value: price,
+        extra_data: downloadLink,
+      }),
+    })
+      .then((response) => {
+        return response.text()
+      })
+      .then((data) => setData(data))
+      .then(console.log(data))
+      .catch((e) => console.log('fetch data error: ', e))
   }
 
   return (
